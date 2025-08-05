acddc01901e0984c0926bdaec12439322d3f435f
RoxiAra <rociomikaelaaramayo@gmail.com>
2025-08-04 22:10:05 -0300

    creacion de contenido temporal en sobre nosotros y cambio minimo en header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,12 @@
+import { Link } from "react-router-dom"
+
 const Header = () => {
   return (
     <header>
       <img src="" alt="" />
       <nav>
         <ul>
-          <li><LInk to="/">Inicio</LInk></li>
+          <li><Link to="/">Home</Link></li>
           <li><Link to="/dashboard">Dashboard</Link></li>
           <li><Link to="/login">Login</Link></li>
           <li><Link to="/register">Register</Link></li>
---
+++ src/pages/SobreNosotros.jsx
@@ -0,0 +1,22 @@
+import { Layout } from "../components/Layout";
+
+const SobreNosotros = () => {
+  return (
+    <Layout>
+      <section>
+        <h1>Deque trata el proyecto</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam, optio.</p>
+      </section>
+      <section>
+        <h1>A quien esta dirigido</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam, optio.</p>
+      </section>
+      <section>
+        <h1>Que tecnologias o enfoques se usaron</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam, optio.</p>
+      </section>
+    </Layout>
+  )
+}
+
+export { SobreNosotros }
